f91e9bc55a96241aa8f84b5cdf656d00812949e1
Aaditya Joshi <cloudaaditya@gmail.com>
2019-11-07 13:06:00 +0545

    update user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,26 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends MY_Controller{
 	
+	public function __construct(){
+		parent::__construct();
+		$this->load->library('form_validation');
+		
+	} 
 
 	public function index(){
-		// echo 'staff list';
-		// $this->load->view( 'pages/list-staff' );
-		$this->load->model( 'User_Model' );
-		$query = $this->User_Model->get( '*' );
-		// var_dump($query);
-		$this->load->view('list-staff', $query);
-	}
-
-	public function add_user(){
-		$this->load->helper('form');
-		$this->load->view('pages/user');
+		$where = array(
+			'role_id' => 2
+		);
+		$data = $this->User_Model->get( '*', $where );
+		$staffs = [
+			'staffs' => $data
+		];
+		$this->load->view('pages/list-user', $staffs);	
 	}
 
 	public function add(){
-
-		$this->load->library('form_validation');
-		$this->load->model( 'User_Model' );
-
 
 		$name = $this->input->post( 'name' );
 		$email = $this->input->post( 'email' );
@@ -37,10 +35,10 @@ class User extends MY_Controller{
 		$this->form_validation->set_rules('displayname', 'Display Name', 'required' );
 
 		if( $this->form_validation->run() ){
-
 			$data = array( 
 				'username' => $name,
 				'password' => $password,
+				'phone_number' => $phone_number,
 				'email' => $email,
 				'display_name' => $displayname,
 				'role_id' => 2
@@ -49,13 +47,53 @@ class User extends MY_Controller{
 			$query =$this->User_Model->save( $data );
 
 			if( $query ){
-				redirect('User');
+				redirect('user');
 			}else{
 				echo "error";
 			}
 
 		}else{
-			$this->load->view( 'pages/user' );
+			$this->load->view( 'pages/add-user' );
 		}
+	}
+
+	public function edit( $id = null ){
+		
+		$where = array(
+			'id' => $id
+		);
+		$data = $this->User_Model->get( '*', $where );
+		$staffs = [
+			'staffs' => $data
+		];
+		$this->load->view('pages/edit-user', $staffs);
+	}
+
+	public function update(){
+
+		$name = $this->input->post( 'name' );
+		$email = $this->input->post( 'email' );
+		$phone_number = $this->input->post( 'number' );
+		$displayname = $this->input->post('displayname');
+
+		$id = $this->input->post('id');
+
+		$data = array( 
+			'username' => $name,
+			'phone_number' => $phone_number,
+			'email' => $email,
+			'display_name' => $displayname,
+		);
+
+		$where = array(
+			'id' => $id
+		);
+
+		$query = $this->User_Model->save( $data, $where );
+		if ( $query ){
+			redirect('user');
+		}
+
+
 	}
 }
