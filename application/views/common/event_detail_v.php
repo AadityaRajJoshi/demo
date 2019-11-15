3a1ddf1712caf79abe374e2d5943a304d55f1163
Pradip <cloudmishal@gmail.com>
2019-11-15 14:47:56 +0545

    event detail page change

--- application/views/common/event_detail_v.php
+++ application/views/common/event_detail_v.php
@@ -1,83 +1,93 @@
 <section class="luft-evt-detail-wrapper">
-<div class="luft-evt-header">
-    <div class="evt-name">
-        <h2>Familjedag</h2>
-        <div class="evt-info">
-            <h4><b>DATE </b> <span>10 juli 2019</span></h4>
-            <h4><b>CITY </b> <span>Askersund</span></h4>
-            <h4> <b>STAFF </b> <span>Tomas, Saber</span></h4>
+
+    <div class="luft-evt-header">
+        <div class="evt-name">
+            <h2>Familjedag</h2>
+            <div class="evt-info">
+                <h4><b>DATE </b> <span>10 juli 2019</span></h4>
+                <h4><b>CITY </b> <span>Askersund</span></h4>
+                <h4> <b>STAFF </b> <span>Tomas, Saber</span></h4>
+            </div>
+        </div>
+
+        <div class="evt-order-number">
+            <h5>ORDERNUMBER <span><span> 12345 </h5>
+            <h5>TOTAL WORKINGTIME <span><span> 11h </h5>
         </div>
     </div>
 
-    <div class="evt-order-number">
-        <h5>ORDERNUMBER <span><span> 12345 </h5>
-        <h5>TOTAL WORKINGTIME <span><span> 11h </h5>
-    </div>
-</div>
+    <div class="evt-time-wrapper">
 
-<div class="evt-time-wrapper">
-    <div class="travel-time-1">
-        <h4>TRAVELTIME 1</h4>
-        <p>07:30-8:30</p>
-    </div>
-    <div class="cs-time">
-        <h4>TRAVELTIME 1</h4>
-        <p>07:30-8:30</p>
-    </div>
-    <div class="evt-time">
-        <h4>TRAVELTIME 1</h4>
-        <p>07:30-8:30</p>
-    </div>
-    <div class="ds-time">
-        <h4>TRAVELTIME 1</h4>
-        <p>07:30-8:30</p>
-    </div>
-    <div class="travel-time-2">
-        <h4>TRAVELTIME 1</h4>
-        <p>07:30-8:30</p>
-    </div>
-</div>
+        <div class="travel-time-1">
+            <h4>TRAVELTIME 1</h4>
+            <p>07:30-8:30</p>
+        </div>
 
-<div class="evt-info">
-    <h4><b> PACKED BY </b> <span> Emma </span></h4>
-    <h4><b> PACKING TIME </b> <span> 1hr </span></h4>
-</div>
+        <div class="cs-time">
+            <h4>TRAVELTIME 1</h4>
+            <p>07:30-8:30</p>
+        </div>
 
-<div class="evt-detail-sect evt-sect-border">
-    <div class="evt-detail-first">
-        <h4>EVENT ADRESS</h4>
-        <p>På gräsmattan Lea Rauch <br />
-        bredvid Hotell Norra Vättern</p>        
-    </div>
-    <div class="evt-contact-second">
-        <h4>CONTACTPERSON</h4>
-        <p>Lea Rauch</p>        
-    </div>
-    <div class="evt-contact-third">
-        <h4>Contact phone</h4>
-        <p>0731-472122</p>        
-    </div>
-    
-</div>
+        <div class="evt-time">
+            <h4>TRAVELTIME 1</h4>
+            <p>07:30-8:30</p>
+        </div>
 
-<div class="evt-detail-sect">
-    <div class="evt-detail-first">
-        <h4>PRODUCTS</h4>
-        <p>Megatrack 15 M <br />
-        Hoppborg Safari <br />
-        Zorb med<br />
-        3 bollar</p>        
-    </div>
-    <div class="evt-contact-second">
-        <h4>ELECTRICITY</h4>
-        <p>Finns ett uttag på hotellet/resturagen</p>        
-    </div>
-    <div class="evt-contact-third">
-        <h4>OTHER INFORMATION</h4>
-        <p>Vi ska städa efter eventet</p>        
-    </div>
-</div>
+        <div class="ds-time">
+            <h4>TRAVELTIME 1</h4>
+            <p>07:30-8:30</p>
+        </div>
 
+        <div class="travel-time-2">
+            <h4>TRAVELTIME 1</h4>
+            <p>07:30-8:30</p>
+        </div>
+    </div>
 
+    <div class="evt-info">
+        <h4><b> PACKED BY </b> <span> Emma </span></h4>
+        <h4><b> PACKING TIME </b> <span> 1hr </span></h4>
+    </div>
 
+    <div class="evt-detail-sect evt-sect-border">
+
+        <div class="evt-detail-first">
+            <h4>EVENT ADRESS</h4>
+            <p>På gräsmattan Lea Rauch <br />
+            bredvid Hotell Norra Vättern</p>        
+        </div>
+
+        <div class="evt-contact-second">
+            <h4>CONTACTPERSON</h4>
+            <p>Lea Rauch</p>        
+        </div>
+
+        <div class="evt-contact-third">
+            <h4>Contact phone</h4>
+            <p>0731-472122</p>        
+        </div>
+        
+    </div>
+
+    <div class="evt-detail-sect">
+
+        <div class="evt-detail-first">
+            <h4>PRODUCTS</h4>
+            <p>Megatrack 15 M <br />
+            Hoppborg Safari <br />
+            Zorb med<br />
+            3 bollar</p>        
+        </div>
+
+        <div class="evt-contact-second">
+            <h4>ELECTRICITY</h4>
+            <p>Finns ett uttag på hotellet/resturagen</p>        
+        </div>
+
+        <div class="evt-contact-third">
+            <h4>OTHER INFORMATION</h4>
+            <p>Vi ska städa efter eventet</p>        
+        </div>
+        
+    </div>
 </section>
