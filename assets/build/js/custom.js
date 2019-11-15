315df4c5a7f4f9fac7e5170c5004d3c2bbd3c8c5
Pradip <cloudmishal@gmail.com>
2019-11-15 10:01:34 +0545

    change image display

--- assets/build/js/custom.js
+++ assets/build/js/custom.js
@@ -13,20 +13,36 @@
 
         $('.custom-styled-select').select2();
 
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#uploaded-image').attr('src', e.target.result);
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        $("#profile-image").change(function () {
+            readURL(this);
+        });
+
+        //status yes no toggle script
+        $(document).on('click', '.onoffswitch-checkbox', function () {
+            var id = $(this).data('id');
+            $.ajax({
+                url: LUFTLEK.ajax_url + LUFTLEK.route.event_toggle_status,
+                type: 'POST',
+                data: { event_id: id },
+            });
+        });
+
     };
 
     /* DOM ready event */
     $(document).ready(documentReadyCallbackFunc);
-    
-    //delete script
-    $( document ).on( 'click', '.onoffswitch-checkbox', function(){
-        var id = $(this).data( 'id' );
-        $.ajax({
-            url : LUFTLEK.ajax_url + LUFTLEK.route.event_toggle_status,
-            type : 'POST',
-            data : {event_id : id },
-        });
-    } );
 
 })(jQuery);
 
