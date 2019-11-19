13a0862cfb6da5e60e2249155abe8c1fa4b3b158
yujesh shrestha <uzess@uzess.(none)>
2019-11-19 14:07:57 +0545

    remove unwanted files

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -147,7 +147,7 @@ class User extends MY_Controller{
     public function add(){
     	
     	if(! is_admin())
-    	$this->invalid_access();
+    		$this->invalid_access();
 
     	$this->data[ 'meta' ] = get_msg('meta_add_staff');
     	$this->data['page'] = 'profile_v';
